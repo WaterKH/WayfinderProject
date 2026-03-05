e54a23604a349666a32cb3302fd1a08d28429e82
WaterKH <waterkh@outlook.com>
2026-03-05 22:49:23 +1300

    fix

--- Domain/Models/BaseMoogleShop.cs
+++ Domain/Models/BaseMoogleShop.cs
@@ -4,7 +4,7 @@ namespace WayfinderProject.Domain.Models
 {
     public class BaseMoogleShopData<TSubData> : BaseData<TSubData>
     {
-        [DisplayInTable(headerName: "Category", iconPath: "areas_gray.png", order: 3, colorClass: "orange")]
+        [DisplayInTable(headerName: "Category", iconPath: "areas_gray.png", order: 3, colorClass: "blue")]
         public string Category { get; set; } = string.Empty;
     }
 }
