c00c726373ccd2b5f6cc6eab27ef789d3c80d552
WaterKH <waterkh@outlook.com>
2026-03-06 12:52:47 +1300

    add lookup music

---
+++ Domain/Models/MemoryArchive/Music.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WayfinderProject.Domain.Models.MemoryArchive
+{
+    public class MusicWrapper
+    {
+        [JsonPropertyName("Music")]
+        public List<Music> Music { get; set; } = new();
+    }
+
+    public class Music
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Link { get; set; } = string.Empty;
+    }
+}
